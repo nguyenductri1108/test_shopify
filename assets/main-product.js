a75a6957f4689d4f71135341958bb8bd913683cb
NgoDucPhu <phund@omegatheme.com>
2023-07-20 08:52:40 +0700

    commit

---
+++ assets/main-product.js
@@ -0,0 +1,22 @@
+tabs = document.querySelectorAll(".tab");
+
+console.log("hehehehe");
+tabContents = document.querySelectorAll(".tab-content");
+
+tabs.forEach(function (tab) {
+    tab.addEventListener("click", function () {
+        contentId = this.dataset.contentId;
+        content = document.getElementById(contentId);
+
+        tabContents.forEach(function (content) {
+            content.classList.remove("active");
+        });
+
+        tabs.forEach(function (tab) {
+            tab.classList.remove("active");
+        });
+
+        this.classList.add("active");
+        content.classList.add("active");
+    });
+});
